0fb34f51cd1e4df65c35d0d9fc82819ea40b1d8e
Prabu <prabucd@gmail.com>
2020-04-25 18:32:06 +0530

    Adding Docker demo project

---
+++ src/main/java/test/Demo/App.java
@@ -0,0 +1,13 @@
+package test.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is demo project \n" );
+    }
+}
